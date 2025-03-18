fcbde03e188de23f913e19d8203906864dd2e00a
juanmanuel2005 <151100168+juanmanuel2005@users.noreply.github.com>
2025-03-18 14:06:40 +0000

    Se ralizo un codigo que imprime una escalera con los numeros has t un numero pedido al usuario

---
+++ ACTIVIDAD_3/escalera_hasta_n.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int main() {
+    int num;
+ 
+    printf("Ingresa un número : ");
+    scanf("%d", &num);
+    printf("\n");
+ 
+    printf("Numeros hasta %d:\n", num);
+    printf("\n");
+ 
+    for (int i = 1; i <= num; i++) {
+        for (int j = 1; j <= i; j++){
+        printf("%d ", j);
+    }
+    printf("\n");
+    }
+ 
+    return 0;
+}
