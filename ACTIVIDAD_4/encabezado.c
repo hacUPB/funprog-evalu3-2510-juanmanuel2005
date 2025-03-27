dacbeda8d9a9cc8b63f9220dc4b7d292a50a02a3
juanmanuel2005 <151100168+juanmanuel2005@users.noreply.github.com>
2025-03-27 13:50:53 +0000

    Se creo directorio de actividad 4 y se realizo un ejemplo de encabezado

---
+++ ACTIVIDAD_4/encabezado.c
@@ -0,0 +1,38 @@
+/******************************************************************************
+                     
+                     UNIVERCIDAD PONTIFICIA BOLIVARIANA
+                     Escuela de Ingenirías
+                     Ingenieria Eléctrica
+                     Juan Manuel Gómez
+                     ID: 000516995
+                     
+
+*******************************************************************************/
+
+#include <stdio.h>
+// Prototipo de la función 
+void encabezado(void);
+//------------------------------------------------------------------------------
+
+int main(int arg, char const *argv[])
+{
+    encabezado();
+
+    return 0;
+}
+
+//------------------------------------------------------------------------------
+//Definición de la función
+void encabezado(void)
+{
+    // Imprimir el encabezado
+    printf("/******************************************************************************\n");
+    printf("\n");
+    printf("                     UNIVERSIDAD PONTIFICIA BOLIVARIANA\n");
+    printf("                     Escuela de Ingenierías\n");
+    printf("                     Ingeniería Eléctrica\n");
+    printf("                     Juan Manuel Gómez\n");
+    printf("                     ID: 000516995\n");
+    printf("\n");
+    printf("*******************************************************************************/\n");
+}
